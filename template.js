456170fec3fbc651a5933d0fcea6d8355a2d95c4
Jason Imison <nosami@users.noreply.github.com>
2014-05-09 11:55:11 +0100

    Update template.js

--- template.js
+++ template.js
@@ -14,7 +14,7 @@ exports.description = 'Create a C# solution with main project + nunit test proje
 exports.notes = '';
 
 // Template-specific notes to be displayed after question prompts.
-exports.after = 'You should now install project dependencies with nuget update /path/to/sln'
+exports.after = 'You should now install project dependencies with nuget restore'
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
